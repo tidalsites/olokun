03d049f0708f71b4f694fd020fce4be8c9aeb616
Devin Haynes <dhaynes916@gmail.com>
2023-12-17 09:38:38 -0500

    Fixed height for map

--- app/components/GoogleMap.tsx
+++ app/components/GoogleMap.tsx
@@ -21,7 +21,7 @@ export const Map = () => {
         zoom={14}
         center={location}
         mapContainerClassName="gmap"
-        mapContainerStyle={{ width: "100%" }}
+        mapContainerStyle={{ width: "100%", height: 400 }}
       >
         <Marker position={location} />
       </GoogleMap>
