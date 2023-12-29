5ab934b5f1e6dab3fb582b381d71763c6fcf7df1
Devin Haynes <dhaynes916@gmail.com>
2023-12-29 11:16:34 -0500

    Added cc avatara img

--- app/completecloud/page.tsx
+++ app/completecloud/page.tsx
@@ -2,8 +2,11 @@ import { Tenor_Sans } from "next/font/google";
 
 const tenor = Tenor_Sans({ weight: "400", subsets: ["latin"] });
 
+import avatara from "@/public/casestudies/avatara.png";
+
 import { Features } from "./features";
 import { CaseStudies } from "./casestudies";
+import Image from "next/image";
 
 export default function CompleteCloud() {
   return (
@@ -14,13 +17,16 @@ export default function CompleteCloud() {
           <h2 className={`${tenor.className} text-4xl text-zinc-800`}>
             A Solution for Everyone
           </h2>
-          <p className="max-w-prose leading-6 py-8">
+          <p className="max-w-prose leading-6 pt-8">
             CompleteCloud is a Smart Platform that was built for security and
             productivity conscious businesses and is delivered and supported in
             an all-inclusive per user per month pricing model, giving our
             customers a competitive advantage and peace of mind.
           </p>
-          <p className="max-w-prose leading-6 py-8">
+          <div className="mt-4 outline -outline-offset-8 outline-2 outline-brand w-fit">
+            <Image src={avatara} alt="avatara dashboard" />
+          </div>
+          <p className="max-w-prose leading-6 py-8 mt-4">
             The CompleteCloud Platform offers a revolutionized approach to
             buying, accessing, and maintaining your IT environment. Key features
             include:
