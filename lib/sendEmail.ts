3162cb8dc3245cab21cfc11b8fb514bc54bcd2a3
Devin Haynes <dhaynes916@gmail.com>
2024-01-06 11:14:13 -0500

    Add server validation to contact email

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,19 +1,52 @@
 "use server";
 
-import { TContactSchema } from "@/schemas/contact";
+import { ContactSchema, TContactSchema } from "@/schemas/contact";
 import { Resend } from "resend";
 
-// import { ContactEmail } from "@/app/components/ContactEmailTemplate";
-// import { createElement } from "react";
+import { ContactEmail } from "@/app/components/ContactEmailTemplate";
+import { createElement } from "react";
+import { CreateEmailResponse } from "resend/build/src/emails/interfaces";
+import { ZodFormattedError } from "zod";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendContactEmail = async (formData: TContactSchema) => {
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "devin@tidalsites.com",
-    subject: "Olokun Contact form",
-    text: "test",
-    // react: createElement(ContactEmail, formData),
-  });
+type ContactResponse = {
+  success: boolean;
+  error?: ZodFormattedError<TContactSchema> | string;
+  data?: CreateEmailResponse;
+};
+
+export const sendContactEmail = async (
+  formData: TContactSchema
+): Promise<ContactResponse> => {
+  const result = ContactSchema.safeParse(formData);
+
+  if (!result.success) {
+    return { success: false, error: result.error.format() };
+  }
+
+  try {
+    const resp = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "devin@tidalsites.com",
+      subject: "Olokun Contact form",
+      // text: "test",
+      react: createElement(ContactEmail, formData),
+    });
+
+    return { success: true, data: resp };
+  } catch (error) {
+    let returnedError;
+
+    if (error instanceof Error) {
+      returnedError = error.message;
+    }
+
+    if (Array.isArray(error)) {
+      returnedError = error.join(",");
+    }
+
+    returnedError = String(error);
+    return { success: false, error: returnedError };
+  }
 };
