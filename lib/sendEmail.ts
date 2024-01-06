6233a45378f4fd52c280a54488a9467a129c4d38
Devin Haynes <dhaynes916@gmail.com>
2024-01-06 10:39:10 -0500

    Testing email on vercel

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -3,8 +3,8 @@
 import { TContactSchema } from "@/schemas/contact";
 import { Resend } from "resend";
 
-import { ContactEmail } from "@/app/components/ContactEmailTemplate";
-import { createElement } from "react";
+// import { ContactEmail } from "@/app/components/ContactEmailTemplate";
+// import { createElement } from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -13,7 +13,7 @@ export const sendContactEmail = async (formData: TContactSchema) => {
     from: "onboarding@resend.dev",
     to: "devin@tidalsites.com",
     subject: "Olokun Contact form",
-    // text: "test",
-    react: createElement(ContactEmail, formData),
+    text: "test",
+    // react: createElement(ContactEmail, formData),
   });
 };
